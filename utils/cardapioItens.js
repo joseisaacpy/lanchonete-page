4fcb26aaed8efe7e186b793bf3e13428e97542e3
José Isaac <joseisaacnascimento@gmail.com>
2025-11-01 22:03:32 -0300

    feat(data): criar arquivo para guardar itens do cardapio

---
+++ utils/cardapioItens.js
@@ -0,0 +1,72 @@
+const cardapioItens = [
+  // Hambúrgueres
+  {
+    id: 1,
+    nome: "X-Burger",
+    categoria: "Hambúrguer",
+    preco: 15,
+    descricao: "Hambúrguer com queijo, alface e tomate",
+    imagem: "/assets/images/hamburguer.png",
+  },
+  {
+    id: 2,
+    nome: "X-Bacon",
+    categoria: "Hambúrguer",
+    preco: 18,
+    descricao: "Hambúrguer com bacon, queijo e molho especial",
+    imagem: "/assets/images/hamburguer.png",
+  },
+  // Sanduíches
+  {
+    id: 3,
+    nome: "Misto Quente",
+    categoria: "Sanduíche",
+    preco: 8,
+    descricao: "Presunto e queijo derretido",
+    imagem: "/assets/images/sanduiche.png",
+  },
+  {
+    id: 4,
+    nome: "Frango Crispy",
+    categoria: "Sanduíche",
+    preco: 16,
+    descricao: "Frango empanado crocante com maionese",
+    imagem: "/assets/images/sanduiche.png",
+  },
+  // Bebidas
+  {
+    id: 5,
+    nome: "Refrigerante Lata",
+    categoria: "Bebida",
+    preco: 5,
+    descricao: "Coca-Cola, Fanta ou Guaraná",
+    imagem: "/assets/images/cola.png",
+  },
+  {
+    id: 6,
+    nome: "Suco Natural",
+    categoria: "Bebida",
+    preco: 7,
+    descricao: "Suco de laranja ou maracujá",
+    imagem: "/assets/images/suco.png",
+  },
+  // Sobremesas
+  {
+    id: 7,
+    nome: "Milkshake Chocolate",
+    categoria: "Sobremesa",
+    preco: 12,
+    descricao: "Milkshake cremoso de chocolate",
+    imagem: "/assets/images/milkshake.png",
+  },
+  {
+    id: 8,
+    nome: "Brownie",
+    categoria: "Sobremesa",
+    preco: 9,
+    descricao: "Brownie de chocolate com pedaços de noz",
+    imagem: "/assets/images/brownie.png",
+  },
+];
+
+export default cardapioItens;
