7ae8aa4d762bbb9b4a2db72bae9763cfec2778b6
José Isaac <joseisaacnascimento@gmail.com>
2025-11-16 14:01:41 -0300

    feat(homepage): criar função para carregar dados da api e montar a página - remover estados não usados - remover icons

--- app/pageClient.tsx
+++ app/pageClient.tsx
@@ -1,38 +1,48 @@
 "use client";
-
 // importa hooks
-import { useState } from "react";
-
-import cardapioItens from "@/utils/cardapioItens";
-
-// importa o CardProduto
+import { useState, useEffect } from "react";
+// importa componentes
 import CardProduto from "../components/CardProduto";
-
-// importa icon
-import { ShoppingCart } from "lucide-react";
-
-// importa header
 import Header from "@/components/Header";
+import Loader from "@/components/Loader";
+// types
+import Produto from "@/app/types/produto";
+import Categoria from "@/app/types/categoria";
+// funções de consumo da API
+import { getProdutos, getCategorias } from "@/lib/api";
 
 export default function Home() {
-  // estado para filtro, por padrao é "Tudo"
+  const [produtos, setProdutos] = useState<Produto[]>([]);
+  const [categorias, setCategorias] = useState<Categoria[]>([]);
   const [filtro, setFiltro] = useState<string>("Tudo");
-  // estado para controlar a quantidade de produtos no carrinho
-  const [quantidade, setQuantidade] = useState<number>(0);
-  // estado para controlar o modal de carrinho
-  const [abrirModal, setAbrirModal] = useState<boolean>(false);
+  const [loader, setLoader] = useState<boolean>(true);
+  // carrega os produtos e categorias ao iniciar o componente
+  useEffect(() => {
+    async function carregarDados() {
+      try {
+        // chama as funções
+        const [prods, cats] = await Promise.all([
+          getProdutos(),
+          getCategorias(),
+        ]);
+        // define os estados
+        setProdutos(prods);
+        setCategorias(cats);
+      } catch (error) {
+        console.error(error);
+      } finally {
+        // fecha o loader
+        setLoader(false);
+      }
+    }
+    // chama a função de carregar os dados
+    carregarDados();
+  }, []);
 
-  // função para filtrar produtos por meio dos botões
-  const produtosFiltrados =
-    filtro === "Tudo"
-      ? cardapioItens
-      : cardapioItens.filter(
-          (produtos) =>
-            produtos.categoria.toLowerCase() === filtro.toLowerCase()
-        );
-
-  // categorias
-  const categorias = ["Tudo", "Hambúrguer", "Sanduíche", "Bebida", "Sobremesa"];
+  // se tiver carregando, mostrar o loader
+  if (loader) {
+    return <Loader />;
+  }
 
   return (
     <section className="px-4 relative">
@@ -41,45 +51,34 @@ export default function Home() {
       <h1 className="mx-2 py-4 text-lg md:text-3xl lg:text-4xl font-bold md:text-center">
         Se liga no nosso <span className="text-red-600">Cardápio</span>!
       </h1>
-      {/* carrinho */}
-      <div className="hidden absolute top-4 right-4">
-        <ShoppingCart size={40} className="text-white" />
-        <span className="absolute bottom-0 left-0 px-2 rounded-full bg-red-600 text-white">
-          {quantidade}
-        </span>
-      </div>
       {/* botoes de filtros */}
       <div className="flex justify-center flex-wrap gap-2 mb-4">
-        {categorias.map((categoria) => {
-          return (
+        {["Tudo", ...categorias.map((categoria) => categoria.nome)].map(
+          (item) => (
             <button
-              key={categoria}
+              key={item}
               className={`btn ${
-                filtro === categoria ? "bg-red-600" : "bg-gray-600"
+                filtro === item
+                  ? "bg-red-default opacity-100"
+                  : "bg-red-default opacity-40"
               }`}
-              onClick={() => setFiltro(categoria)}
+              onClick={() => setFiltro(item)}
             >
-              {categoria}
+              {item}
             </button>
-          );
-        })}
+          )
+        )}
       </div>
       {/* grid de produtos */}
       <div className="grid gap-6 grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-        {produtosFiltrados.length === 0 && (
-          <p className="text-center col-span-4">Nenhum produto encontrado</p>
-        )}
         {/* map de produtos */}
-        {produtosFiltrados.map((produto) => {
-          return (
-            <div
-              key={produto.id}
-              className="transition-all duration-300 ease-in-out transform opacity-0 scale-95 animate-fadeIn"
-            >
-              <CardProduto {...produto} />
-            </div>
-          );
-        })}
+        {produtos
+          .filter((produto) => {
+            return filtro === "Tudo" || produto.categoria.nome === filtro;
+          })
+          .map((produto) => {
+            return <CardProduto key={produto.id} {...produto} />;
+          })}
       </div>
     </section>
   );
