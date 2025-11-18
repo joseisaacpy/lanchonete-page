0d5fce4fe1caa99ee269ae9323862781a11c885d
José Isaac <joseisaacnascimento@gmail.com>
2025-11-18 13:08:06 -0300

    style(footer): corrigir cor do footer para variável css

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="w-full flex items-center justify-center bg-green-600 p-4 px-8">
+    <footer className="w-full flex items-center justify-center bg-green-default p-4 px-8">
       <p className="text-white text-center font-bold text-lg">
         Lanchonete Fullstack 2025
       </p>
