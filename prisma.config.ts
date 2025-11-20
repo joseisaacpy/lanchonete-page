5a76e4587da1034fa9c7814d22589f156ee83d91
José Isaac <joseisaacnascimento@gmail.com>
2025-11-19 21:47:02 -0300

    fix(prisma): ajustar configurações do prisma

--- prisma.config.ts
+++ prisma.config.ts
@@ -2,13 +2,12 @@ import { defineConfig, env } from "prisma/config";
 
 // importa o dotenv
 import dotenv from "dotenv";
-dotenv.config();
+if (process.env.NODE_ENV !== "production") {
+  dotenv.config();
+}
 
 export default defineConfig({
   schema: "prisma/schema.prisma",
-  migrations: {
-    path: "prisma/migrations",
-  },
   engine: "classic",
   datasource: {
     url: env("DATABASE_URL"),
