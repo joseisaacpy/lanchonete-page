a43c8c2085bdf06cdd7c57c994194d2146385a94
José Isaac <joseisaacnascimento@gmail.com>
2025-11-20 14:25:06 -0300

    refactor(categorias): refatorar método para pegar as categorias

--- app/categorias/page.tsx
+++ app/categorias/page.tsx
@@ -1,32 +1,24 @@
-import Categoria from "../types/categoria";
 import Link from "next/link";
+import { prisma } from "@/lib/prisma";
 
 // importa o CardCategoria
 import CardCategoria from "@/components/CardCategoria";
 
-// função para buscar categorias
-async function getCategorias(): Promise<Categoria[]> {
-  try {
-    const response = await fetch("/api/categorias", {
-      cache: "no-cache", // evita o cache
-    });
-    return response.json();
-  } catch (error) {
-    console.error(error);
-    return []; // retorna um array vazio
-  }
-}
-
 export default async function Categorias() {
   // faz a requisição para buscar as categorias
-  const categorias = await getCategorias();
+  const categorias = await prisma.categoria.findMany();
   return (
-    <section className="p-4 flex flex-col gap-2">
-      <h1 className="text-2xl font-bold">Lista de categorias</h1>
+    <section className="section-container">
+      <h1 className="h1-title">Lista de categorias</h1>
       <p>Quantidade de categorias: {categorias.length}</p>
       <p>
         Deseja adicionar uma nova categoria?
-        <Link className="ml-1 text-red-default underline" href="/categorias/nova">Clique aqui</Link>
+        <Link
+          className="ml-1 text-red-default underline"
+          href="/categorias/nova"
+        >
+          Clique aqui
+        </Link>
       </p>
       <div className="grid gap-4 grid-cols-2 md:grid-cols-4">
         {categorias.map((categoria) => {
