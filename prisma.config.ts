f4b409a0e2d79dfe0fce9dd12787937a63c7602c
José Isaac <joseisaacnascimento@gmail.com>
2025-11-20 13:57:37 -0300

    remove(prisma config): remover import do dotenv

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,10 +1,5 @@
+import "dotenv/config";
 import { defineConfig, env } from "prisma/config";
-
-// importa o dotenv
-import dotenv from "dotenv";
-if (process.env.NODE_ENV !== "production") {
-  dotenv.config();
-}
 
 export default defineConfig({
   schema: "prisma/schema.prisma",
