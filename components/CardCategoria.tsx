16764e4293cbbd87fce3f042deff7ce55c38c0ee
José Isaac <joseisaacnascimento@gmail.com>
2025-11-23 11:50:06 -0300

    feat(cardCategoria): adicionar estado que armazena quantidade de produtos - criar função que consome api para pegar qtde de produtos de uma cateogria

--- components/CardCategoria.tsx
+++ components/CardCategoria.tsx
@@ -4,6 +4,7 @@ import { Trash2, Edit } from "lucide-react";
 import { toast } from "sonner";
 import { useRouter } from "next/navigation";
 import Categoria from "@/types/categoria";
+import { useState, useEffect } from "react";
 
 import { Button } from "@/components/ui/button";
 import {
@@ -16,6 +17,27 @@ import {
 
 export default function CardCategoria({ id, nome }: Categoria) {
   const router = useRouter();
+
+  const [quantidadeProdutos, setQuantidadeProdutos] = useState<number>(0);
+
+  useEffect(() => {
+    // função para pegar quantidade de produtos da categoria
+    async function getQuantidadeProdutos() {
+      try {
+        const response = await fetch(`/api/categorias/${id}`, {
+          cache: "no-cache", // evita o cache
+        });
+        const data = await response.json();
+        setQuantidadeProdutos(data.quantidadeProdutos);
+      } catch (error) {
+        console.error(error);
+        toast.error("Ocorreu um erro ao buscar a quantidade de produtos");
+      }
+    }
+
+    getQuantidadeProdutos();
+  });
+
   // função para deletar uma categoria
   const handleDelete = async () => {
     try {
@@ -34,7 +56,11 @@ export default function CardCategoria({ id, nome }: Categoria) {
     <Card className="w-full max-w-sm">
       <CardHeader>
         <CardTitle>{nome}</CardTitle>
-        <CardDescription>x produtos cadastrados</CardDescription>
+        <CardDescription>{`${
+          quantidadeProdutos === 1
+            ? "1 produto cadastrado"
+            : `${quantidadeProdutos} produtos cadastrados`
+        }`}</CardDescription>
       </CardHeader>
       <CardFooter className="flex flex-col gap-2">
         <Button onClick={handleDelete} className="w-full">
