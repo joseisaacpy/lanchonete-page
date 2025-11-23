d4d048e4dc43dc972bb66d0c6b8b2d627a2e1ab7
José Isaac <joseisaacnascimento@gmail.com>
2025-11-22 21:53:21 -0300

    feat(cardCategoria): adicionar função para lidar com o delete

--- components/CardCategoria.tsx
+++ components/CardCategoria.tsx
@@ -1,17 +1,33 @@
-// importa icones
-import { Trash2, Edit } from "lucide-react";
+"use client";
 
-// tpye da categoria
+import { Trash2, Edit } from "lucide-react";
+import { toast } from "sonner";
+import { useRouter } from "next/navigation";
+
 import Categoria from "@/types/categoria";
 
 export default function CardCategoria({ id, nome }: Categoria) {
+  const router = useRouter();
+
+  const handleDelete = async () => {
+    try {
+      await fetch(`/api/categorias/${id}`, {
+        method: "DELETE",
+      });
+      toast.success("Categoria deletada com sucesso");
+      router.refresh();
+    } catch (error) {
+      console.error(error);
+      toast.error("Ocorreu um erro ao deletar a categoria");
+    }
+  };
   return (
     <div key={id} className="bg-gray-200 rounded-md p-4 text-black">
       <h2 className="font-bold">{nome}</h2>
       <footer>
         {/* botões */}
         <div className="flex flex-row gap-2">
-          <button className="btn bg-red-default">
+          <button onClick={handleDelete} className="btn bg-red-default">
             <Trash2 />
           </button>
           <button className="btn bg-red-default">
