489e710a463f3236d639a172ee2f0a477043bb8a
José Isaac <joseisaacnascimento@gmail.com>
2025-11-25 17:44:53 -0300

    fix(layout): corrigir erro de toast duplicado

--- app/(public)/layout.tsx
+++ app/(public)/layout.tsx
@@ -1,16 +1,9 @@
 "use client";
 
-import { Toaster } from "sonner";
-
 export default function LayoutClient({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return (
-    <>
-      <Toaster position="top-right" />
-      {children}
-    </>
-  );
+  return <>{children}</>;
 }
